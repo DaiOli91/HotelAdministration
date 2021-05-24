5eb8156e47da3f98bcf6c0a27da73a99bc79799d
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-05-24 20:23:39 -0300

    Hotel class- first methods

--- System/src/Controller/Hotel.java
+++ System/src/Controller/Hotel.java
@@ -1,4 +1,48 @@
 package Controller;
 
+import Model.Booking;
+import Model.User;
+//import Model.Booking;
+import Model.Room;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hotel {
+    private List<User> users;
+    private List<Booking> roomBookings;
+    private List<Room> rooms;
+
+    public Hotel() {
+        this.users = new ArrayList<>();
+        this.roomBookings = new ArrayList<>();
+        this.rooms = new ArrayList<>();
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public List<Booking> getRoomBookings() {
+        return roomBookings;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public String getUsersData() {
+        return users.toString();
+    }
+
+    public String getRoomBookingsData() {
+        return roomBookings.toString();
+    }
+
+    public String getRoomsData() {
+        return rooms.toString();
+    }
+    // ╔═══════════════════════════════ End User Methods
+    // ╠═══════════════════════════════ Administrative Methods
+    // ╚═══════════════════════════════ Management Methods
 }
