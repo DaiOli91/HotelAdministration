f7e15f0963674af091713942837753ef5ebe309f
Walter Moretti <wally.moretti@gmail.com>
2021-05-24 20:24:19 -0300

    'description' variable and getter added

--- System/src/Model/Shift.java
+++ System/src/Model/Shift.java
@@ -2,14 +2,24 @@ package Model;
 
 public enum Shift {
 
-    MORNING("Morning"),
-    AFTERNOON("Afternoon"),
-    NIGHT("Night");
+    MORNING("Morning", "05:00am - 13:00pm"),
+    AFTERNOON("Afternoon", "13:00pm - 21:00pm"),
+    NIGHT("Night", "21:00pm - 05:00am");
 
     private String shift;
+    private String description;
 
-    private Shift(String shift) {
+    private Shift(String shift, String description) {
 
         this.shift = shift;
+        this.description = description;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
