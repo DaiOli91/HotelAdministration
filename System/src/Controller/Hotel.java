2fd5d490274d9bda835297a78293864a5b649314
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-05-25 16:56:43 -0300

    encapsuled methods within repository classes to divide work- neater

--- System/src/Controller/Hotel.java
+++ System/src/Controller/Hotel.java
@@ -1,104 +1,80 @@
 package Controller;
 
 import Model.*;
+import Repository.BookingRepository;
+import Repository.RoomRepository;
+import Repository.UserRepository;
 //import Model.Booking;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Hotel {
-    private List<User> users;
-    private List<Booking> roomBookings;
-    private List<Room> rooms;
+    private UserRepository users;
+    private BookingRepository bookings;
+    private RoomRepository rooms;
+
+
 
     public Hotel() {
-        this.users = new ArrayList<>();
-        this.roomBookings = new ArrayList<>();
-        this.rooms = new ArrayList<>();
+        this.users = new UserRepository();
+        this.bookings = new BookingRepository();
+        this.rooms = new RoomRepository();
+        this.users.userHC();
+
     }
 
     public List<User> getUsers() {
-        return users;
+        return users.getUsers();
+    }
+    public String getUsersData() {
+        return users.getUsersData();
     }
 
-    public List<Booking> getRoomBookings() {
-        return roomBookings;
+    public List<Booking> getBookings() {
+        return bookings.getRoomBookings();
+    }
+
+    public String getBookingsData() {
+        return bookings.getRoomBookingsData();
     }
 
     public List<Room> getRooms() {
-        return rooms;
-    }
-
-    public String getUsersData() {
-        return users.toString();
-    }
-
-    public String getRoomBookingsData() {
-        return roomBookings.toString();
+        return rooms.getRooms();
     }
 
     public String getRoomsData() {
-        return rooms.toString();
+        return rooms.getRoomsData();
     }
 
-    // ╔═══════════════════════════════ End User Methods
-    //TODO Password should be 10 characters long
-    public String register(User user) {
-        String message = "User has been added successfully";
-        if (this.users.isEmpty()) {
-            this.addSuperAdmin();
-            this.users.add(user);
-        } else if (this.users.contains(user)) {
-            message = "User already exists";
-        } else this.users.add(user);
-
-        return message;
-    }
-
-    public void addSuperAdmin() {
-        User superAdmin = new Manager("11111111"
-                , "Super"
-                , "Admin"
-                , 100
-                , Gender.FEMALE
-                , "Something 123"
-                , "111111111"
-                , "superadmin@mail.com"
-                , "superadmin123");
-
-        this.users.add(superAdmin);
-    }
-
-    public String logIn(User user) {
-        //TODO fix log in
+    public String logInHotel(String DNI, String pass){
         String message = "User not found. Please, try again or register.";
-        if (!this.users.isEmpty()) {
-            for (User aux_user : users
-            ) {
-                if ((aux_user.getDni().equals(user.getDni()))
-                        && (aux_user.getPassword().equals(user.getPassword()))) {
-                    message = "Welcome, " + user.getFirstName();
-                    if (user instanceof Manager) {
-                        message.concat(this.menuManager());
-                    } else if (user instanceof Receptionist) {
-                        message.concat(this.menuReceptionist());
-                    } else message.concat(this.menuPassenger());
-                }
-
-            }
-
+        User aux_user = new Passenger(DNI, pass);
+        aux_user = this.users.logIn(aux_user);
+        if (aux_user != null) {
+            message = "Welcome, " + aux_user.getFirstName();
+            if (aux_user instanceof Manager) {
+                message.concat(this.menuManager());
+            } else if (aux_user instanceof Receptionist) {
+                message.concat(this.menuReceptionist());
+            } else message.concat(this.menuPassenger());
         }
         return message;
     }
 
+    public String register(User user){
+        return this.users.add(user);
+    }
+
+
+
     // ╠═══════════════════════════════ Administrative Methods
     // ╚═══════════════════════════════ Management Methods
-
 
     //TODO menuManager
     public String menuManager() {
         //insert code here
-       return "Manager Menu";
+        return "Manager Menu";
     }
 
     //TODO menuReceptionist
@@ -113,71 +89,4 @@ public class Hotel {
         return "Passenger Menu";
     }
 
-    public void userHC() {
-        addSuperAdmin();
-        this.users.add(new Passenger("14785969"
-                , "Andrea"
-                , "Carrizo"
-                , 60
-                , Gender.FEMALE
-                , "Gaboto 5646"
-                , "4585858"
-                , "something@asomething.com"
-                , "pass123"
-                , "Mar del Plata"));
-
-        this.users.add(new Passenger("14874623"
-                , "Felipe"
-                , "Graziano"
-                , 60
-                , Gender.MALE
-                , "Mexico 3536"
-                , "4651236"
-                , "something@something.com"
-                , "pass123"
-                , "San Clemente"));
-
-        this.users.add(new Receptionist("18956565"
-                , "Sofia"
-                , "Caceres"
-                , 58
-                , Gender.FEMALE
-                , "Bahia Blanca 123"
-                , "2235686968"
-                , "soficaceres@gmail.com"
-                , "sofi1966"
-                , Shift.MORNING));
-
-        this.users.add(new Receptionist("35236598"
-                , "Santiago"
-                , "Gonzalez"
-                , 30
-                , Gender.MALE
-                , "Rio Negro 456"
-                , "2235456985"
-                , "santig@gmail.com"
-                , "santi30"
-                , Shift.NIGHT));
-
-        this.users.add(new Manager("13525252"
-                , "Calos"
-                , "Patriarcado"
-                , 61
-                , Gender.MALE
-                , "Patagones 6000"
-                , "2235505065"
-                , "carlos.patriarcado@gmail.com"
-                , "patriarcado123"));
-
-        this.users.add(new Manager("25525252"
-                , "Natalia"
-                , "Bossy"
-                , 40
-                , Gender.FEMALE
-                , "San Luis 4052"
-                , "2235235689"
-                , "natybossy@gmail.com"
-                , "bossy123"));
-
-    }
 }
