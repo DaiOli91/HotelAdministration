b1e4a3c0f6fe909985a3b6bf7066bf4cc585cda4
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-05-25 16:55:41 -0300

    modified accordingly for testing

--- System/src/Main.java
+++ System/src/Main.java
@@ -5,7 +5,6 @@ import Model.*;
 public class Main {
     public static void main(String[] args) {
         Hotel hotel1 = new Hotel();
-        hotel1.userHC();
         //System.out.println(hotel1.getUsersData());
         User user1 = new Manager("35892293"
                 , "Daiana"
@@ -17,10 +16,10 @@ public class Main {
                 , "oliveratup@gmail.com"
                 , "pass456");
 
-        System.out.println(hotel1.logIn(user1));
+        System.out.println(hotel1.logInHotel(user1.getDni(), user1.getPassword()));
         System.out.println(hotel1.register(user1));
         System.out.println(hotel1.getUsersData());
-        System.out.println(hotel1.logIn(user1));
+        System.out.println(hotel1.logInHotel(user1.getDni(), user1.getPassword()));
 
     }
 }
