407c39a50999a0767189abc97b2e274fb0655578
Walter Moretti <wally.moretti@gmail.com>
2021-05-25 19:30:50 -0300

    Implementando IRepository. Ni ganas de escribirlo en Ingles.

--- System/src/Repository/RoomRepository.java
+++ System/src/Repository/RoomRepository.java
@@ -5,8 +5,10 @@ import Model.Room;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
 
 public class RoomRepository implements IRepository<Room, Integer> {
+
     private List<Room> rooms;
 
     public RoomRepository() {
@@ -16,27 +18,34 @@ public class RoomRepository implements IRepository<Room, Integer> {
     public List<Room> getRooms() {
         return rooms;
     }
-    public String getRoomsData() {
-        return rooms.toString();
-    }
 
     @Override
     public String add(Room room) {
-        return "";
+
+        rooms.add(room);
+        return "The room was added correctly";
     }
 
     @Override
-    public void delete(Integer integer) {
+    public void delete(Integer roomNumber) {
 
+        rooms.remove((Room) rooms.stream().filter(room1 -> room1.getNumber() == roomNumber));
     }
 
     @Override
-    public Room search(Integer integer) {
-        return null;
+    public Room search(Integer roomNumber) {
+
+        Room room = (Room) rooms.stream().filter(room1 -> room1.getNumber() == roomNumber);
+        return room;
     }
 
     @Override
     public void edit(Room room) {
 
+        System.out.println("No se que carajo hacer aca. La clase no se edita y la chupan todos. Odio la facultad");
+    }
+
+    public String getRoomsData() {
+        return rooms.toString();
     }
 }
