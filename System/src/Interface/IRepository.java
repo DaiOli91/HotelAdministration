444a8ba3206f974a56c3663ad166612025e5e5ed
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-05-25 18:48:44 -0300

    methods edited- generic interface

--- System/src/Interface/IRepository.java
+++ System/src/Interface/IRepository.java
@@ -1,9 +1,9 @@
 package Interface;
 
-public interface IRepository<T> {
+public interface IRepository<T, J> {
 
-    public void add(T t);
-    public void delete(String string);
-    public T search(String string);
-    public T edit(T t);
+    public String add(T t);
+    public void delete(J j);
+    public T search(J j);
+    public void edit(T t);
 }
