5ae1be4a187434fc2f919836ecdee45c31fa2493
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-05-29 16:59:53 -0300

    add method edited

--- System/src/Interface/IRepository.java
+++ System/src/Interface/IRepository.java
@@ -2,7 +2,7 @@ package Interface;
 
 public interface IRepository<T, J> {
 
-    public String add(T t);
+    public void add(T t);
     public void delete(J j);
     public T search(J j);
     public void edit(T t);
