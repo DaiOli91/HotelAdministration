a1e16a5c9efdd105174c0f6e34945c9745fd4738
Walter Moretti <wally.moretti@gmail.com>
2021-05-29 19:56:27 -0300

    Added 'MenuPassenger' class

---
+++ System/src/menues/MenuPassenger.java
@@ -0,0 +1,14 @@
+package menues;
+
+import controller.Hotel;
+import model.User;
+
+import java.util.Scanner;
+
+public class MenuPassenger {
+
+    public static void menuPassenger(Scanner scan, Hotel OlivandersHotel, User loggedUser) {
+
+
+    }
+}
