9700aa3f5b024b4dc22ae0f4b1f6616bd3d6d4fb
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-05-30 13:08:20 -0300

    new methods and validations for booking

--- System/src/controller/Hotel.java
+++ System/src/controller/Hotel.java
@@ -157,16 +157,23 @@ public class Hotel {
 
     // ╠═══════════════════════════════ Booking Methods // 'ABML' order
     public String createBooking(Booking booking) {
-        //TODO logic here to find available dates. Also set TotalCost and AmountOfNights in booking
-        String message = "So far, booking created successfully";
-        Room aux_room = this.rooms.search(booking.getIdRoom());
-        booking.setTotalCost(aux_room.getCategory().getPrice() * booking.getAmountOfNights()); //TODO set approppriate price
-        this.bookings.add(booking);
+        //TODO set TotalCost and AmountOfNights in booking
+        String message = "Booking created successfully";
+        List<Booking> checkBookings = this.getActiveBookingsByRoomAndDate(booking.getStartDate(), booking.getEndDate(), booking.getIdRoom());
+        if(checkBookings.isEmpty()) {
+            Room aux_room = this.rooms.search(booking.getIdRoom());
+            booking.setTotalCost(aux_room.getCategory().getPrice() * booking.getAmountOfNights()); //TODO set approppriate price
+            this.bookings.add(booking);
+        } else {
+            message = "The room is not available for those dates. Please choose another room"; //TODO return message in Exception
+        }
         return message;
     }
 
     public boolean deleteBooking(Integer idBooking) {
         //TODO logic not to cancel booking on the same day- at least 48hs
+        Booking aux_booking = this.bookings.search(idBooking);
+
         return this.bookings.delete(idBooking);
     }
 
@@ -190,6 +197,11 @@ public class Hotel {
                 message = "Booking found. Passenger CheckedIn";
                 aux_booking.setState(State.CHECKED);
                 aux_booking = this.bookings.edit(aux_booking);
+
+                Room aux_room = this.rooms.search(aux_booking.getIdRoom()); //change the availability of the current list of rooms
+                aux_room.setAvailability(Availability.OCCUPIED);
+
+                this.rooms.edit(aux_room);
             } else {
                 message = "Passenger's ID and Booking's Id do not match";
             }
@@ -274,8 +286,98 @@ public class Hotel {
 
     }
 
+    public List<Booking> getAllBookings(){
+        return this.bookings.getRoomBookings();
+    }
+
+    /***
+     * Checks only MainPassengerId
+     * @param userDni
+     * @return list of all bookings that contains that DNI as main passenger ID
+     */
+    public List<Booking> getBookingsByUser(String userDni){
+        return this.bookings
+                .getRoomBookings()
+                .stream()
+                .filter(b -> b
+                        .getIdMainPassenger()
+                        .equals(userDni))
+                .collect(Collectors.toList());
+    }
+
+    /***
+     * Checks only MainPassengerId and filters by state
+     * @param userDni
+     * @return list of all bookings that contains that DNI as main passenger ID
+     */
+    public List<Booking> getActiveBookingsByUser(String userDni){
+        return this.bookings
+                .getRoomBookings()
+                .stream()
+                .filter(b -> b
+                        .getIdMainPassenger()
+                        .equals(userDni))
+                .filter(b -> b.getState().equals("ACTIVE"))
+                .collect(Collectors.toList());
+    }
+
+    public List<Booking> getBookingsByState(State state){
+        return this.bookings
+                .getRoomBookings()
+                .stream()
+                .filter(b -> b
+                        .getState()
+                        .equals(state))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     *
+     * @return List of all active bookings for a Specific room- to validate
+     */
+    public List<Booking> getActiveBookingsByRoom(Integer roomId){
+        return this.bookings
+                .getRoomBookings()
+                .stream()
+                .filter(b -> b
+                        .getIdRoom() == roomId)
+                .filter(b -> (b.getState().equals("ACTIVE")) || (b.getState().equals("CHECKED")))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     *
+     * @return List of all active bookings for specific Room in a specific period of time- to validate
+     */
+    public List<Booking> getActiveBookingsByRoomAndDate(LocalDate startDate, LocalDate endDate, Integer idRoom){
+        return this.getActiveBookingsByRoom(idRoom)
+                .stream()
+                .filter(b -> ((b.getStartDate().isAfter(startDate))
+                        && (b.getStartDate().isBefore(endDate))
+                ||((b.getEndDate().isAfter(startDate))
+                        && (b.getEndDate().isBefore(endDate)))))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     *
+     * @return List of all active bookings for specific Room in a specific period of time- to validate
+     */
+    public List<Booking> getActiveBookingsByDate(LocalDate startDate, LocalDate endDate){
+        return this.bookings.getRoomBookings()
+                .stream()
+                .filter(b -> ((b.getStartDate().isAfter(startDate))
+                        && (b.getEndDate().isBefore(endDate))))
+                .collect(Collectors.toList());
+    }
+
+
+
+
 
     // ╠═══════════════════════════════ Room Methods // 'ABML' order
+
+    //TODO getAvailableRoomsToday / filters only by Availability, in case I want to rent the room today
     // ╚═══════════════════════════════ HC Methods
     public void addSuperAdmin() {
         User superAdmin = new Manager("11111111"
