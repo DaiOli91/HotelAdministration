f0f491005ac4ddb6af23d263ca1c3e905ad6eb59
Walter Moretti <wally.moretti@gmail.com>
2021-05-29 22:38:18 -0300

    Added empty "MenuReceptionist" class

---
+++ System/src/menues/MenuManager.java
@@ -0,0 +1,14 @@
+package menues;
+
+import controller.Hotel;
+import model.User;
+
+import java.util.Scanner;
+
+public class MenuManager {
+
+    public static void menuManager(Scanner scan, Hotel OlivandersHotel, User loggedUser) {
+
+
+    }
+}
