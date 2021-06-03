e131157ac3ae26457b4d14795ec571583e8c5a61
Walter Moretti <wally.moretti@gmail.com>
2021-06-03 11:40:54 -0300

    Modified menu to implement methods

--- System/src/menues/MenuManager.java
+++ System/src/menues/MenuManager.java
@@ -3,39 +3,54 @@ package menues;
 import controller.Hotel;
 import model.User;
 
+import javax.naming.event.ObjectChangeListener;
 import java.util.Scanner;
 
 public class MenuManager {
 
-    public static void menuManager(Scanner scan, Hotel OlivandersHotel, User loggedUser) {
+    public static void menuManager(Scanner scan, Hotel OllivandersHotel, User loggedUser) {
 
         int z = 0, option = 0;
 
         while (z == 0) {
 
             System.out.println("\nMenu Manager\n==========\n");
-            System.out.println("1. \n2. \n3. ");
-            System.out.println("4. Test - Edit Account");
+            System.out.println("1. Add Employee\n2. See Users\n3. Edit Account\n4. Deactivate User");
+            System.out.println("5. See Rooms\n6. Edit Room\n7 . Deactivate Room");
             System.out.println("0. Log Out");
             System.out.print("Option: ");
             System.out.flush();
+            //TODO Need to implement "InputMismatchException".
             option = scan.nextInt();
             switch (option) {
                 case 1: {
-                    System.out.println("\nInsert text here\n");
+                    System.out.println("\nAdd Employee\n");
                     break;
                 }
                 case 2: {
-                    System.out.println("\nInsert text 2 here\n");
+                    System.out.println("\nSee Users\n");
                     break;
                 }
                 case 3: {
-                    System.out.println("\nInsert text 3 here\n");
+                    System.out.println("\nEdit Account\n");
                     break;
                 }
                 case 4: {
-                    System.out.println("\nThis is a test\n");
-                    MenuEditAccount.menuEditAccount(scan, OlivandersHotel, loggedUser);
+                    System.out.println("\nDeactivate User\n");
+                    break;
+                }
+                case 5: {
+                    System.out.println("\nSee Rooms\n");
+
+                    MenuSeeRooms.menuSeeRooms(scan, OllivandersHotel);
+                    break;
+                }
+                case 6: {
+                    System.out.println("\nEdit Room\n");
+                    break;
+                }
+                case 7: {
+                    System.out.println("\nDeactivate Room\n");
                     break;
                 }
                 case 0: {
