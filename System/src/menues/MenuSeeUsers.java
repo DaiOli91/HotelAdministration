fceeec57c638049b2c08056c6fb99f9e7e84e63e
Walter Moretti <wally.moretti@gmail.com>
2021-06-04 15:43:52 -0300

    Implemented more 'get' methods from "Hotel" class

--- System/src/menues/MenuSeeUsers.java
+++ System/src/menues/MenuSeeUsers.java
@@ -13,9 +13,8 @@ public class MenuSeeUsers {
         while (z == 0) {
 
             System.out.println("\nMenu See Users\n==============\n");
-            System.out.println("1. See All Users");
-            System.out.println("2. See All Employees\n3. See Active Employees\n4. See Former Employees");
-            System.out.println("5. See All Passenger\n6. See Active Passengers\n7. See Former Passenger");
+            System.out.println("1. See All Employees\n2. See Active Employees\n3. See Former Employees");
+            System.out.println("4. See All Passenger\n5. See Active Passengers\n6. See Former Passenger");
             System.out.println("0. Back");
             System.out.print("Option: ");
             System.out.flush();
@@ -23,38 +22,33 @@ public class MenuSeeUsers {
             option = scan.nextInt();
             switch (option) {
                 case 1: {
-                    System.out.println("\nSee All Users\n");
-                    System.out.println("\n" + OllivandersHotel.getUsers() + "\n");
+                    System.out.println("\nSee All Employees\n");
+                    System.out.println("\n" + OllivandersHotel.getAllEmployees() + "\n");
                     break;
                 }
                 case 2: {
-                    System.out.println("\nSee All Employees\n");
-                    // TODO Needs a new method.
-                    break;
-                }
-                case 3: {
                     System.out.println("\nSee Active Employees\n");
                     System.out.println("\n" + OllivandersHotel.getActiveEmployees() + "\n");
                     break;
                 }
-                case 4: {
+                case 3: {
                     System.out.println("\nSee Former Employees\n");
                     System.out.println("\n" + OllivandersHotel.getFormerEmployees() + "\n");
                     break;
                 }
-                case 5: {
+                case 4: {
                     System.out.println("\nSee All Passenger\n");
                     System.out.println("\n" + OllivandersHotel.getPassengers() + "\n");
                     break;
                 }
-                case 6: {
+                case 5: {
                     System.out.println("\nSee Active Passengers\n");
-                    // TODO Needs a new method.
+                    System.out.println("\n" + OllivandersHotel.getActivePassengers() + "\n");
                     break;
                 }
-                case 7: {
+                case 6: {
                     System.out.println("\nSee Former Passenger\n");
-                    // TODO Needs a new method.
+                    System.out.println("\n" + OllivandersHotel.getFormerPassengers() + "\n");
                     break;
                 }
                 case 0: {
