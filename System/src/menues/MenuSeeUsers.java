3ef06b03ac9c4372adbff443f05f263117924b25
Walter Moretti <wally.moretti@gmail.com>
2021-06-05 11:09:47 -0300

    Added case for "Search User.." methods

--- System/src/menues/MenuSeeUsers.java
+++ System/src/menues/MenuSeeUsers.java
@@ -15,6 +15,7 @@ public class MenuSeeUsers {
             System.out.println("\nMenu See Users\n==============\n");
             System.out.println("1. See All Employees\n2. See Active Employees\n3. See Former Employees");
             System.out.println("4. See All Passenger\n5. See Active Passengers\n6. See Former Passenger");
+            System.out.println("7. Search User by Full Name\n");
             System.out.println("0. Back");
             System.out.print("Option: ");
             System.out.flush();
@@ -28,7 +29,8 @@ public class MenuSeeUsers {
                 }
                 case 2: {
                     System.out.println("\nSee Active Employees\n");
-                    System.out.println("\n" + OllivandersHotel.getActiveEmployees() + "\n");
+                    System.out.println("\n" + OllivandersHotel.getActiveManagers() + "\n");
+                    System.out.println("\n" + OllivandersHotel.getActiveReceptionists() + "\n");
                     break;
                 }
                 case 3: {
@@ -49,6 +51,31 @@ public class MenuSeeUsers {
                 case 6: {
                     System.out.println("\nSee Former Passenger\n");
                     System.out.println("\n" + OllivandersHotel.getFormerPassengers() + "\n");
+                    break;
+                }
+                case 7: {
+                    System.out.println("\nSearch User by Full Name\n");
+                    System.out.print("1. Search User by ID, 2. Search User by Full Name: ");
+                    int searchOption = scan.nextInt();
+
+                    if (searchOption == 1) {
+
+                        System.out.print("Enter User ID: ");
+                        String dni = scan.next();
+
+                        System.out.println("\n" + OllivandersHotel.getUserByID(dni) + "\n");
+                    } else if (searchOption == 2) {
+
+                        System.out.print("Enter First Name: ");
+                        String firstName = scan.next();
+                        System.out.print("Enter Last Name: ");
+                        String lastName = scan.next();
+
+                        System.out.println("\n" + OllivandersHotel.getUserByFullName(firstName, lastName) + "\n");
+                    } else {
+
+                        System.out.println("\nNot a valid option\n");
+                    }
                     break;
                 }
                 case 0: {
