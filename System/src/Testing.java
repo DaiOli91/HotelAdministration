4843762276e596e9f2c59cc4f4c3dc11f6d67120
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-06-07 19:18:29 -0300

    HC methods updated

--- System/src/Testing.java
+++ System/src/Testing.java
@@ -1,16 +1,11 @@
 import controller.Hotel;
 import model.*;
-import repository.BookingRepository;
-import repository.RoomRepository;
-import repository.UserRepository;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 public class Testing {
 
@@ -30,11 +25,16 @@ public class Testing {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // Testing.roomHC(roomRepo.getRooms());
-        // Testing.userHC(userRepo.getUsers());
-        // Testing.bookingHC(bookingRepo.getRoomBookings());
-/*
+        // Testing.roomHC(newHotel);
+       //  Testing.userHC(newHotel);
+       // Testing.bookingHC(newHotel);
+        try {
+            newHotel.saveData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+/*
         try {
             roomRepo.readGson();
          //   System.out.println(roomRepo.getRooms().toString());
@@ -55,36 +55,73 @@ public class Testing {
         } catch (IOException e) {
             e.printStackTrace();
         }
-*/
 
         try {
             newHotel.loadData();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        */
 
-        System.err.println(newHotel.getAvailableRooms(LocalDate.of(2021, 06, 17), LocalDate.of(2021, 07, 03)));
+        //System.err.println(newHotel.getUsers().size());
+      //  System.err.println(newHotel.getAvailableRooms(LocalDate.of(2021, 06, 17), LocalDate.of(2021, 07, 03)));
+     //   System.out.println(newHotel.getBookings().toString());
+     //   System.err.println("AVAILABLE: \n" + getActiveBookingsInPeriod(LocalDate.of(2021, 06, 17), LocalDate.of(2021, 07, 3), newHotel.getBookings()));
+       // System.err.println(ChronoUnit.DAYS.between(LocalDate.of(2021, 06, 21), LocalDate.of(2021, 05, 21)));
+        System.out.println("Ultimo ID: " + newHotel.getLastRoomNumber());
+        newHotel.createBooking(135, "38530953", "", LocalDate.of(2021, 7, 15), LocalDate.of(2021, 8,1));
         System.out.println(newHotel.getBookings().toString());
-        System.err.println("AVAILABLE: \n" + getActiveBookingsInPeriod(LocalDate.of(2021, 06, 17), LocalDate.of(2021, 07, 3), newHotel.getBookings()));
-        //System.out.println(ChronoUnit.DAYS.between(LocalDate.of(2021, 06, 21), LocalDate.of(2021, 06, 21)));
+        try {
+            newHotel.saveData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
-    public static void roomHC(List<Room> rooms) {
-        rooms.add(new Room(Category.EXECUTIVE, Availability.FREE));
-        rooms.add(new Room(Category.GUEST, Availability.FREE));
-        rooms.add(new Room(Category.JUNIOR, Availability.FREE));
-        rooms.add(new Room(Category.PRESIDENTIAL, Availability.FREE));
-        rooms.add(new Room(Category.GUEST, Availability.CLEANING));
-        rooms.add(new Room(Category.GUEST, Availability.OCCUPIED));
-        rooms.add(new Room(Category.GUEST, Availability.IN_DESINFECTION));
-        rooms.add(new Room(Category.GUEST, Availability.OUT_OF_SERVICE));
-        rooms.add(new Room(Category.EXECUTIVE, Availability.FREE));
-        rooms.add(new Room(Category.GUEST, Availability.FREE));
-        rooms.add(new Room(Category.JUNIOR, Availability.FREE));
-        rooms.add(new Room(Category.PRESIDENTIAL, Availability.FREE));
+    public static void roomHC(Hotel hotel) {
+        List<Room> rooms = hotel.getRooms();
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.EXECUTIVE, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.JUNIOR, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.PRESIDENTIAL, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.CLEANING));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.OCCUPIED));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.IN_DESINFECTION));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.OUT_OF_SERVICE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.EXECUTIVE, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.JUNIOR, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.PRESIDENTIAL, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.EXECUTIVE, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.JUNIOR, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.PRESIDENTIAL, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.CLEANING));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.OCCUPIED));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.IN_DESINFECTION));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.OUT_OF_SERVICE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.EXECUTIVE, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.JUNIOR, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.PRESIDENTIAL, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.EXECUTIVE, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.JUNIOR, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.PRESIDENTIAL, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.CLEANING));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.OCCUPIED));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.IN_DESINFECTION));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.OUT_OF_SERVICE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.EXECUTIVE, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.GUEST, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.JUNIOR, Availability.FREE));
+        rooms.add(new Room(hotel.getLastRoomNumber()+1, Category.PRESIDENTIAL, Availability.FREE));
     }
 
-    public static void userHC(List<User> users) {
+    public static void userHC(Hotel hotel) {
+        List<User> users = hotel.getUsers();
         users.add(new Passenger("14874804"
                 , "Andrea"
                 , "Carrizo"
@@ -151,42 +188,41 @@ public class Testing {
                 , "santi30"
                 , Shift.NIGHT));
 
-        users.add(new Manager("13525252"
-                , "Calos"
-                , "Patriarcado"
-                , 61
-                , Gender.MALE
-                , "Patagones 6000"
-                , "2235505065"
-                , "carlos.patriarcado@gmail.com"
-                , "patriarcado123"));
-
-        users.add(new Manager("25525252"
-                , "Natalia"
-                , "Bossy"
-                , 40
+        users.add(new Manager("35892293"
+                , "Daiana"
+                , "Olivera"
+                , 30
                 , Gender.FEMALE
                 , "San Luis 4052"
+                , "2235506592"
+                , "daiolivera@gmail.com"
+                , "pass123"));
+
+        users.add(new Manager("39098436"
+                , "Walter"
+                , "Moretti"
+                , 25
+                , Gender.MALE
+                , "Dardo Rocha 70"
                 , "2235235689"
-                , "natybossy@gmail.com"
-                , "bossy123"));
+                , "wally.moretti@gmail.com"
+                , "pass123"));
     }
 
-    public static void bookingHC(List<Booking> bookings) {
-        // LocalDate startDate = LocalDate.now();
-        //  LocalDate endDate = LocalDate.now().plusDays(7);
-        DateTimeFormatter format = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
-        bookings.add(new Booking(101, "38530953", "14874804", LocalDate.now(), LocalDate.now().plusDays(7)));
-        bookings.add(new Booking(101, "14589623", "14874804", LocalDate.now().plusMonths(1), LocalDate.now().plusMonths(1).plusDays(7)));
-        bookings.add(new Booking(102, "14874804", "14589623", LocalDate.now().plusDays(5), LocalDate.now().plusDays(5).plusDays(7)));
-        bookings.add(new Booking(102, "14589623", "14874804", LocalDate.now().plusDays(20), LocalDate.now().plusDays(20).plusDays(7)));
-        bookings.add(new Booking(103, "14874804", "38530953", LocalDate.now(), LocalDate.now().plusDays(7)));
-        bookings.add(new Booking(103, "38530953", "14874804", LocalDate.now().plusDays(10), LocalDate.now().plusDays(10).plusDays(7)));
+    public static void bookingHC(Hotel hotel) {
+        List<Booking> bookings = hotel.getBookings();
+        //DateTimeFormatter format = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+        bookings.add(new Booking(hotel.getLastBookingId()+1, 101, "38530953", "14874804", LocalDate.now(), LocalDate.now().plusDays(7)));
+        bookings.add(new Booking(hotel.getLastBookingId()+1, 101, "14589623", "14874804", LocalDate.now().plusMonths(1), LocalDate.now().plusMonths(1).plusDays(7)));
+        bookings.add(new Booking(hotel.getLastBookingId()+1, 102, "14874804", "14589623", LocalDate.now().plusDays(5), LocalDate.now().plusDays(5).plusDays(7)));
+        bookings.add(new Booking(hotel.getLastBookingId()+1, 102, "14589623", "14874804", LocalDate.now().plusDays(20), LocalDate.now().plusDays(20).plusDays(7)));
+        bookings.add(new Booking(hotel.getLastBookingId()+1, 103, "14874804", "38530953", LocalDate.now(), LocalDate.now().plusDays(7)));
+        bookings.add(new Booking(hotel.getLastBookingId()+1, 103, "38530953", "14874804", LocalDate.now().plusDays(10), LocalDate.now().plusDays(10).plusDays(7)));
         //cancelBooking(3);
 
     }
+/*
 
-    /*
         public static List<Booking> getActiveBookingsInPeriod(LocalDate startPeriod, LocalDate endPeriod, List<Booking> bookings) {
             return bookings.stream().filter(b -> !dateIsBetweenPeriod(b.getStartDate(), startPeriod, endPeriod))
                     .filter(b->(!dateIsBetweenPeriod(b.getEndDate(), startPeriod, endPeriod)))
