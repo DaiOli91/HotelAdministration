8781e76911b2546e76f3a017f10833ea99d094c2
wally <wally.moretti@gmail.com>
2021-06-08 13:43:38 -0300

    Modified exception and fixed implementation

---
+++ System/src/exception/UserActiveDeactiveException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class UserActiveDeactiveException extends Exception {
+    public UserActiveDeactiveException(String message) {
+        super(message);
+    }
+}
--- System/src/exception/UserIsAlreadyActive.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class UserIsAlreadyActive extends Exception{
-    public UserIsAlreadyActive(){
-        super("User account is already Active");
-    }
-}
