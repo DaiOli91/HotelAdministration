6e3133af377220984dd504ed5e3526b8353c181d
wally <wally.moretti@gmail.com>
2021-06-08 13:45:05 -0300

    Fixed validations and implemented try-catch

--- System/src/util/Log.java
+++ System/src/util/Log.java
@@ -1,6 +1,8 @@
 package util;
 
 import controller.Hotel;
+import exception.BookingNotFoundException;
+import exception.DateValidationException;
 import model.Passenger;
 import model.User;
 
@@ -10,13 +12,33 @@ import java.util.List;
 public class Log {
 
     public static User logIn(String dni, String Password, Hotel hotel) throws IOException {
-        hotel.loadData();
+
+        try {
+            hotel.loadData();
+        } catch (DateValidationException e) {
+            System.out.println("\n" + e.getMessage() + "\n");
+        } catch (BookingNotFoundException e) {
+            System.out.println("\n" + e.getMessage() + "\n");
+        }
 
         User user = new Passenger(dni, Password);
         User returnUser = null;
 
-        if (!hotel.getUsers().isEmpty()) {
+        if (hotel.getUsers().size() != 0) {
 
+            returnUser = hotel.getUsers()
+                    .stream()
+                    .filter(user1 -> user1.getDni().equals(user.getDni()) && user1.getPassword().equals(user.getPassword()))
+                    .findFirst().orElse(null);
+
+            if (returnUser != null) {
+
+                if (!returnUser.getActive()) {
+
+                    returnUser = null;
+                }
+            }
+            /*
             for (User aux_user : hotel.getUsers()) {
 
                 if ((aux_user.getDni().equals(user.getDni())) && (aux_user.getPassword().equals(user.getPassword()))) {
@@ -27,6 +49,7 @@ public class Log {
                     }
                 }
             }
+            */
         }
 
         return returnUser;
